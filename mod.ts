3cd53944d4c0fc6d4c8ac643b62b03eca020f2e1
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-06-17 14:07:45 +0100

    feat(eslint-config): add tailwindcss plugin to eslint config
    This commit adds the eslint-plugin-tailwindcss to the eslint
    configuration. It also modifies the antfu function call to include
    the tailwindcss plugin configuration. This will help in linting
    tailwindcss specific syntax and rules.

--- mod.ts
+++ mod.ts
@@ -1,5 +1,6 @@
 import path from "pathe";
 import antfu from "@antfu/eslint-config";
+import tailwind from "eslint-plugin-tailwindcss";
 import { meta } from "eslint-plugin-svelte";
 
 type UserConfigs = Parameters<typeof antfu>[1];
@@ -19,36 +20,42 @@ type ESLintConfig = ReturnType<typeof antfu>;
 export function ryoppippi(...args: UserConfigs[]): ESLintConfig {
   meta.name satisfies string;
 
-  return antfu({
-    formatters: true,
-    svelte: true,
-    unocss: true,
-    yaml: true,
-    markdown: true,
-    typescript: {
-      tsconfigPath: path.join(import.meta.dirname, "tsconfig.json"),
-    },
-    stylistic: {
-      indent: "tab",
-      quotes: "single",
-      semi: true,
-    },
-  }, /** general rules */ {
-    rules: {
-      /* eslint rules */
-      "eqeqeq": ["error", "always", { null: "ignore" }],
-      "no-unexpected-multiline": "error",
-      "no-unreachable": "error",
-    },
-  }, /* svelte rules */ {
-    files: ["*.svelte"],
-    rules: {
-      "svelte/indent": ["error", {
+  return antfu(
+    {
+      formatters: true,
+      svelte: true,
+      unocss: true,
+      yaml: true,
+      markdown: true,
+      typescript: {
+        tsconfigPath: path.join(import.meta.dirname, "tsconfig.json"),
+      },
+      stylistic: {
         indent: "tab",
-        alignAttributesVertically: true,
-      }],
-      "svelte/html-self-closing": ["error", "all"],
-      "svelte/sort-attributes": "error",
+        quotes: "single",
+        semi: true,
+      },
     },
-  }, ...args);
+    /** general rules */ {
+      rules: {
+        /* eslint rules */
+        "eqeqeq": ["error", "always", { null: "ignore" }],
+        "no-unexpected-multiline": "error",
+        "no-unreachable": "error",
+      },
+    },
+    /* svelte rules */ {
+      files: ["*.svelte"],
+      rules: {
+        "svelte/indent": ["error", {
+          indent: "tab",
+          alignAttributesVertically: true,
+        }],
+        "svelte/html-self-closing": ["error", "all"],
+        "svelte/sort-attributes": "error",
+      },
+    },
+    ...tailwind.config["flat/default"],
+    ...args,
+  );
 }
