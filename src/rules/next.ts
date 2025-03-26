487bbd708eb6859d6ae6c7db7fc48cc5aad34228
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-03-26 15:04:36 +0000

    chore: fix build

--- src/rules/next.ts
+++ src/rules/next.ts
@@ -1,8 +1,9 @@
 /* eslint-disable ts/no-unsafe-member-access */
 /* eslint-disable ts/no-unsafe-assignment */
+import type { TypedFlatConfigItem } from '@antfu/eslint-config';
 import { interopDefault } from '@antfu/eslint-config';
 
-export async function next(enabled = false) {
+export async function next(enabled = false): Promise<TypedFlatConfigItem[]> {
 	if (!enabled) {
 		return [];
 	}
