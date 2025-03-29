2ab7ccb74d8cb553c0af0cdd329d3683ede857b9
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-03-29 01:09:24 +0000

    feat(tanstack-router): ensure package is installed

--- src/rules/tanstackRouter.ts
+++ src/rules/tanstackRouter.ts
@@ -1,11 +1,13 @@
 import type { TypedFlatConfigItem } from '@antfu/eslint-config';
 
-import { interopDefault } from '@antfu/eslint-config';
+import { ensurePackages, interopDefault } from '@antfu/eslint-config';
 
 export async function tanstackRouter(enabled: boolean = false): Promise<TypedFlatConfigItem[]> {
 	if (!enabled) {
 		return [];
 	}
+
+	await ensurePackages(['@tanstack/eslint-plugin-query']);
 
 	const pluginTanstackRouter = await interopDefault(import('@tanstack/eslint-plugin-router'));
 	return [
