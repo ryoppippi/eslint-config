b2aa896858ab867be3c45e351d9c3b015aa4bc81
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-03-29 01:08:08 +0000

    feat(next): ensure pacakge is installed

--- src/rules/next.ts
+++ src/rules/next.ts
@@ -1,12 +1,14 @@
 /* eslint-disable ts/no-unsafe-member-access */
 /* eslint-disable ts/no-unsafe-assignment */
 import type { TypedFlatConfigItem } from '@antfu/eslint-config';
-import { interopDefault } from '@antfu/eslint-config';
+import { ensurePackages, interopDefault } from '@antfu/eslint-config';
 
 export async function next(enabled = false): Promise<TypedFlatConfigItem[]> {
 	if (!enabled) {
 		return [];
 	}
+
+	await ensurePackages(['@next/eslint-plugin-next']);
 
 	// @ts-expect-error no dts
 	const nextPlugin: any = await interopDefault(import('@next/eslint-plugin-next'));
