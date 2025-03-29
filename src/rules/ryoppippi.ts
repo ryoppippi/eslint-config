83064814d9888969f0ccb391ca2d16547445fa3c
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-03-29 01:07:30 +0000

    feat(ryoppippi): ensure package is installed

--- src/rules/ryoppippi.ts
+++ src/rules/ryoppippi.ts
@@ -1,11 +1,13 @@
 import type { TypedFlatConfigItem } from '@antfu/eslint-config';
 
-import { interopDefault } from '@antfu/eslint-config';
+import { ensurePackages, interopDefault } from '@antfu/eslint-config';
 
 export async function ryoppippi(enabled: boolean = false): Promise<TypedFlatConfigItem[]> {
 	if (!enabled) {
 		return [];
 	}
+
+	await ensurePackages(['eslint-plugin-ryoppippi']);
 
 	const pluginRyoppippi = await interopDefault(import('eslint-plugin-ryoppippi'));
 	return [
